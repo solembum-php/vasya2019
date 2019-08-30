4f44753edc7189b981f269f248e8d823ba0143dd
Vasyl <admin@gmail.com>
2019-08-30 18:30:15 +0300

    creaate files index.php and config.php

---
+++ config.php
@@ -0,0 +1,1 @@
+const SITENAME = 'Сайт Василия';
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+include_once config.php;
+?>
+<!doctype html>
+<html>
+	<head>
+		<title>SITENAME</title>
+	</head>
+	<body>
+		<h1>SITENAME</h1>
+	<body>
+</html>
